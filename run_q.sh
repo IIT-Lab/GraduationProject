4f47ad05afb00072046c7cb1cff2bc7a6ffbd537
Jian-Yin-Shine <1262094391@qq.com>
2019-04-03 15:06:41 +0800

    script

---
+++ run_q.sh
@@ -0,0 +1,5 @@
+python mec.py --num-ue 3 >> log_q.txt 
+python mec.py --num-ue 4 >> log_q.txt
+python mec.py --num-ue 5 >> log_q.txt
+python mec.py --num-ue 6 >> log_q.txt
+python mec.py --num-ue 7 >> log_q.txt
